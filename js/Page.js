7373bb0221c789d67f65adcdd5296ae415f8ccae
MiniRaccoonDog <atorres@spoonflower.com>
2018-05-23 08:49:27 -0400

    added pagination, which finally works, css isn't great, only one search enabled

--- js/Page.js
+++ js/Page.js
@@ -5,14 +5,14 @@ class Page {
     this.array = array
   }
   stepThrough () {
-    console.log('stepthro')
+    // console.log('stepthro')
     const htmlArray = []
     // console.log(this.array)
     const originalArray = this.array
-    console.log('original array?', originalArray)
+    // console.log('original array?', originalArray)
     // console.log(typeof (originalArray))
     for (var i = 0; i < originalArray.length; i++) {
-      console.log(originalArray[i])
+      // console.log(originalArray[i])
       let eachObject = originalArray[i]
       let eachPreview = new Preview(eachObject)
       htmlArray.push(eachPreview.createDivBlock())
@@ -34,19 +34,38 @@ class Page {
   scaleSlicing (htmlArray) {
     if (window.matchMedia('(min-width: 700px)').matches) {
       /* the viewport is at least 700 pixels wide */
-      return this.arraySlicer(24, htmlArray)
+      return this.arraySlicer(20, htmlArray)
     } else {
       /* the viewport is less than 700 pixels wide */
-      return this.arraySlicer(8, htmlArray)
+      return this.arraySlicer(9, htmlArray)
     }
   }
   // length of this for how many
 
+  // paginator (n) {
+  //   const arrayforHTML = this.stepThrough()
+  //   const eachPage = this.scaleSlicing(arrayforHTML)
+  //   return eachPage[n]
+  // }
+
   paginator (n) {
     const arrayforHTML = this.stepThrough()
-    const eachPage = this.scaleSlicing(arrayforHTML)
-    return eachPage[n]
+    const pageArrays = this.scaleSlicing(arrayforHTML)
+    return pageArrays
   }
 }
 
+// this can go in Page.js
+/*
+function pageDiv (n) {
+  console.log('availiable here?', window.pageArray)
+  const pagelinks = []
+  for (let i = 1; i <= n; i++) {
+    pagelinks.push(`<li><a href="" id=${i}>${i}</a></li>`)
+  }
+  $('#pages-top').html(pagelinks.join(''))
+  $('#pages-bottom').html(pagelinks.join(''))
+  return pagelinks
+}
+*/
 export default Page
--- js/script.js
+++ js/script.js
@@ -2,3 +2,87 @@ import Page from './Page'
 import Preview from './Preview'
 import $ from 'jquery'
 import request from 'superagent'
+
+const URL = `https://itunes.apple.com/search?term=`
+// const entireDivArray
+$(document).ready(function () {
+  $('#music-search').submit(function (event) {
+    event.preventDefault()
+    let inputs = $(this).serializeArray()
+    this.reset()
+    const querytype = inputs[0].value
+    const queryvalue = inputs[1].value
+    const searchterm = queryvalue.toLowerCase().replace(/\s+/g, '+')
+    switch (querytype) {
+      case 'search':
+        console.log('general search')
+        generalSearch(searchterm)
+        break
+      case 'artist':
+        console.log('artist called')
+        // artistSearch(searchterm)
+        break
+      case 'album':
+        // albumSearch(searchterm)
+        console.log('album called')
+        break
+      case 'song':
+        // albumSearch(searchterm)
+        console.log('song called')
+        break
+    }
+  })
+  /*
+  $('.nav__music').hover(function () {
+    console.log(this)
+    $(this).children('.nav__bar').removeClass('none')
+    // $(this).nextAll().removeClass('none')
+    console.log($(this).nextAll())
+    // })
+  })
+  */
+  $('.goto').click(function () {
+    console.log(this)
+  })
+})
+
+function generalSearch (query) {
+  request.get(URL + query + `&media=music`)
+    .then(function (response) {
+      const responseObject = JSON.parse(response.text)
+      const page = new Page(responseObject.results)
+      window.pageArray = page.paginator()
+      // entireDivArray
+      const numberOfPages = window.pageArray.length
+      pageDiv(numberOfPages)
+      $('.grid').html(window.pageArray[0])
+      activateNav()
+    })
+}
+
+// this can go in Page.js
+function pageDiv (n) {
+  console.log('availiable here?', window.pageArray)
+  const pagelinks = []
+  for (let i = 1; i <= n; i++) {
+    pagelinks.push(`<li><a href="" id=${i}>${i}</a></li>`)
+  }
+  $('#pages-top').html(pagelinks.join(''))
+  $('#pages-bottom').html(pagelinks.join(''))
+  return pagelinks
+}
+
+function activateNav () {
+  $('#pages-top a').click(function (event) {
+    event.preventDefault()
+    console.log(this)
+    let newpage = $(this).html()
+    $('.grid').html(window.pageArray[(newpage - 1)])
+  })
+  $('#pages-bottom a').click(function (event) {
+    event.preventDefault()
+    console.log(this)
+    let newpage = $(this).html()
+    $('.grid').html(window.pageArray[(newpage - 1)])
+  })
+}
